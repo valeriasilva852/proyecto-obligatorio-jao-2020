75f74973895cf1677145d5d212fdca62916a291c
valeriasilva852 <68906440+valeriasilva852@users.noreply.github.com>
2020-08-11 13:09:11 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,3 +4,26 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function myFunctionValidacion(valor){ // esta funcion es para validacion de campos vacios 
+   if(valor == ""){
+     document.getElementById("label_email").style.display = "block"; //es para que se muestre el mensaje campo vacio
+   }else {
+    document.getElementById("label_email").style.display = "none"; // para escondaerlo mientras no hay accione
+   }
+}
+function myFunctionPassword(valor){
+    if(valor == ""){
+        document.getElementById("label_pass").style.display = "block";
+    }else{
+        document.getElementById("label_pass").style.display = "none";
+    }
+}
+
+function navegacionWeb(){  // esta funcion hace que pueda navegra por la pagina 
+    var usuario = document.getElementById("email").value; //crea valriable email con valor de lo que esta adentro del campo
+    var password = document.getElementById("pass").value;
+    if( (usuario != "") && (password != "") ){
+        window.location.assign('index.html'); // redirecciona al index
+    }
+}
