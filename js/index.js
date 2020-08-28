db5a72490b378d042e18816c306f277316db33f1
valeriasilva852 <68906440+valeriasilva852@users.noreply.github.com>
2020-08-28 20:12:10 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var user = localStorage.getItem('user');
+var user = localStorage.getItem('user'); // consultar el valor del usuario logiado
 if(!user){
-    window.location.href = 'login.html';
+    window.location.href = 'login.html'; //si no hay un usuario logeado que vaya a logiarse
 }else{
     console.log(user);
 }
